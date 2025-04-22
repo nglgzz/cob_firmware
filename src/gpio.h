f5f6e41516a374a4f709791791a809250ce0937f
Angelo Gazzola <angelo@nglgzz.com>
2025-04-22 22:50:19 +0200

    cleanup

--- src/gpio.h
+++ src/gpio.h
@@ -2,55 +2,69 @@
 
 #include <stdint.h>
 
+// https://docs.nordicsemi.com/bundle/ps_nrf52840/page/gpio.html
 #define GPIO0_BASE 0x50000000U
 #define GPIO_PIN_CNF_OFFSET 0x700U
 #define GPIO_PIN_CNF_SIZE 4U
 
 struct gpio {
-  volatile uint32_t OUT,  // (0x504) Write GPIO port
-      OUTSET,             // (0x508) Set individual bits in GPIO port
-      OUTCLR,             // (0x50C) Clear individual bits in GPIO port
-      IN,                 // (0x510) Read GPIO port
-      DIR,                // (0x514) Direction of GPIO pins
-      DIRSET,             // (0x518) DIR set register
-      DIRCLR,             // (0x51C) DIR clear register
-      LATCH,  // (0x520) Latch register indicating what GPIO pins that have met
-              // the criteria set in the PIN_CNF[n].SENSE registers
-      DETECTMODE;  // (0x524) Select between default DETECT signal behavior and
-                   // LDETECT mode
+  //  (0x504) GPIO output register (1 sets pin to high, 0 to low)
+  volatile uint32_t OUT;
+  //  (0x508) GPIO output set register: set individual bits in GPIO port (1 sets
+  //  pin to high, 0 has no effect)
+  volatile uint32_t OUTSET;
+  //  (0x50C) GPIO output clear register: clear individual bits in GPIO port (1
+  //  sets pin to low, 0 has no effect)
+  volatile uint32_t OUTCLR;
+  // (0x510) Read GPIO port
+  volatile uint32_t IN;
+  // (0x514) Direction of GPIO pins
+  volatile uint32_t DIR;
+  // (0x518) DIR set register
+  volatile uint32_t DIRSET;
+  // (0x51C) DIR clear register
+  volatile uint32_t DIRCLR;
+  // (0x520) Latch register indicating what GPIO pins that have met the criteria
+  // set in the PIN_CNF[n].SENSE registers. When reading 1 = criteria has been
+  // met, 0 = criteria has not been met. When writing 1 = clear latch
+  volatile uint32_t LATCH;
+  // (0x524) Select between the default DETECT signal behavior and LDETECT mode
+  volatile uint32_t DETECTMODE;
 };
 
 extern struct gpio *GPIO0;
 
+// ---------------------
+// GPIO_CNF[n]
+// ---------------------
 enum {
-  GPIO_DIR_INPUT,  // Configure pin as input pin
-  GPIO_DIR_OUTPUT  // Configure pin as output pin
+  GPIO_CNF_DIR_INPUT,  // 0: Configure pin as input pin
+  GPIO_CNF_DIR_OUTPUT  // 1: Configure pin as output pin
 };
 enum {
-  GPIO_INPUT_CONNECT,    // Connect input buffer
-  GPIO_INPUT_DISCONNECT  // Disconnect input buffer
+  GPIO_CNF_INPUT_CONNECT,    // 0: Connect input buffer
+  GPIO_CNF_INPUT_DISCONNECT  // 1: Disconnect input buffer
 };
 enum {
-  GPIO_PULL_DISABLED = 0,  // No pull
-  GPIO_PULL_PULLDOWN = 1,  // Pull down on pin
-  GPIO_PULL_PULLUP = 3     // Pull up on pin
+  GPIO_CNF_PULL_DISABLED = 0,  // 0: No pull
+  GPIO_CNF_PULL_PULLDOWN = 1,  // 1: Pull down on pin
+  GPIO_CNF_PULL_PULLUP = 3     // 3: Pull up on pin
 };
-// Ignoring GPIO_DRIVE_* as I don't need it.
+// Ignoring GPIO_CNF_DRIVE_* as I don't need it.
 enum {
-  GPIO_SENSE_DISABLED = 0,
-  GPIO_SENSE_HIGH = 2,  // Sense for high level
-  GPIO_SENSE_LOW = 3    // Sense for low level
+  GPIO_CNF_SENSE_DISABLED = 0,  // 0: Disabled
+  GPIO_CNF_SENSE_HIGH = 2,      // 2: Sense for high level
+  GPIO_CNF_SENSE_LOW = 3        // 3: Sense for low level
 };
 
 struct gpio_pin_cnf {
-  uint8_t DIR,  // Pin direction. Same physical register as DIR register
-      INPUT,    // Connect or disconnect input buffer
-      PULL,     // Pull configuration
-      SENSE;    // Pin sensing mechanism
+  uint8_t DIR;    // Pin direction. Same physical register as DIR register
+  uint8_t INPUT;  // Connect or disconnect input buffer
+  uint8_t PULL;   // Pull configuration
+  uint8_t SENSE;  // Pin sensing mechanism
 };
 
-void gpio_set_pin_cnf(unsigned int base, uint8_t pin,
-                      struct gpio_pin_cnf *config);
+void gpio0_set_pin_cnf(uint8_t pin, struct gpio_pin_cnf *config);
 
 #define GPIO_H
 #endif  // GPIO_H
--- src/nrf52840.h
+++ src/nrf52840.h
@@ -59,15 +59,6 @@ typedef enum {
   FPU_IRQn = 38
 } IRQn_Type;
 
-// -------
-// GPIO
-// -------
-
-// https://www.mouser.com/datasheet/2/297/nRF52840_OPS_v0.5-1074816.pdf
-// (page 158)
-// GPIO port 0
-#define GPIO_BASE 0x50000000U
-
 // Volatile tells the compiler to not optimize code related to this variable, as
 // it's value can be modified by something other than the code that uses it.
 // https://stackoverflow.com/a/246148
@@ -75,88 +66,12 @@ typedef enum {
 // Non volatile pointer to volatile data.
 // https://stackoverflow.com/a/9736812
 //
-// GPIO output register: write GPIO port (1 sets pin to high, 0 to low)
-#define GPIO_OUT (*(volatile unsigned int *)(GPIO_BASE + 0x504))
-
-// GPIO output set register: set individual bits in GPIO port (1 sets pin to
-// high, 0 has no effect)
-#define GPIO_OUTSET (*(volatile unsigned int *)(GPIO_BASE + 0x508))
-
-// GPIO output clear register: clear individual bits in GPIO port (1 sets pin to
-// low, 0 has no effect)
-#define GPIO_OUTCLR (*(volatile unsigned int *)(GPIO_BASE + 0x50C))
-
-// GPIO direction register: direction of GPIO pins (0 set pin as input, 1 set
-// pin as output)
-#define GPIO_DIR (*(volatile unsigned int *)(GPIO_BASE + 0x514))
-
-// Latch register indicating what GPIO pins that have met the criteria set in
-// the PIN_CNF[n].SENSE registers. When reading 1 = criteria has been met, 0 =
-// criteria has not been met. When writing 1 = clear latch
-#define GPIO_LATCH (*(volatile unsigned int *)(GPIO_BASE + 0x520))
-
-// Select between the default DETECT signal behavior and LDETECT mode
-#define GPIO_DETECTMODE (*(volatile unsigned int *)(GPIO_BASE + 0x524))
-
-// Bit number when setting values to the above registers
-// Buttons
-#define SW_PIN_1 11  // 0x5000072C
-#define SW_PIN_2 12  // 0x50000730
-#define SW_PIN_3 24  // 0x50000760
-#define SW_PIN_4 25  // 0x50000764
-
-#define SW_MASK (1 << SW_PIN_1 | 1 << SW_PIN_2 | 1 << SW_PIN_3 | 1 << SW_PIN_4)
-// Leds
-#define LED_PIN_1 13
-#define LED_PIN_2 14
-#define LED_PIN_3 15
-#define LED_PIN_4 16
-
-// Configuration for pin 0
-#define PIN_CNF_OFFSET 0x700U
-// Configuration size
-#define PIN_CNF_SIZE 4U
-/**
- * PIN_CNF[n] - Configuration of GPIO pin `n`:
- *
- * - A[0] DIR: 0 = input, 1 = output (same as DIR register)
- * - B[1] INPUT: 0 = connect input buffer, 1 =  disconnect input buffer
- * - C[2,3] PULL: 0 = no pull, 1 = pulldown, 3 = pullup
- * - D[8,9,10] DRIVE: ...
- * - E[16,17] SENSE: 0 = disabled, 2 = sense high level, 3 = sense low level
- */
-#define PIN_CNF(pin)                                       \
-  (*(volatile unsigned int *)(GPIO_BASE + PIN_CNF_OFFSET + \
-                              (pin * PIN_CNF_SIZE)))
-// Bit to set for configuration
-#define PIN_CNF_DIR 0
-#define PIN_CNF_INPUT 1
-#define PIN_CNF_PULL 2
-#define PIN_CNF_SENSE 16
-
-#define CNF_SENSE_HIGH                                         \
-  (0 << PIN_CNF_DIR | 0 << PIN_CNF_INPUT | 3 << PIN_CNF_PULL | \
-   3 << PIN_CNF_SENSE)
-#define CNF_SENSE_LOW                                          \
-  (0 << PIN_CNF_DIR | 0 << PIN_CNF_INPUT | 3 << PIN_CNF_PULL | \
-   2 << PIN_CNF_SENSE)
 
 // -------
 // GPIOTE
 // -------
 // https://docs.nordicsemi.com/bundle/ps_nrf52840/page/gpiote.html
 #define GPIOTE_BASE 0x40006000U
-
-/**
- * In order to prevent spurious interrupts from the PORT event while configuring
- * the sources, the following must be performed:
- *
- *   1. Disable interrupts on the PORT event (through INTENCLR.PORT).
- *   2. Configure the sources (PIN_CNF[n].SENSE).
- *   3. Clear any potential event that could have occurred during configuration
- *      (write '0' to EVENTS_PORT).
- *   4. Enable interrupts (through INTENSET.PORT).
- */
 
 // Event generated from multiple input GPIO pins with SENSE mechanism enabled
 // (1 = event generated, 0 = event not generated, only one bit is used).
