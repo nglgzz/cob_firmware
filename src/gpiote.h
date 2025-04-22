857cdc4f8b91b20b7b72febf1f0e30e0f2ec9568
Angelo Gazzola <angelo@nglgzz.com>
2025-04-22 22:50:19 +0200

    cleanup

--- src/gpiote.h
+++ src/gpiote.h
@@ -1,5 +1,6 @@
 #ifndef GPIOTE_H
 
+// https://docs.nordicsemi.com/bundle/ps_nrf52840/page/gpiote.html
 #define GPIOTE_BASE 0x40006000U
 
 #define GPIOTE_H
--- src/switches.c
+++ src/switches.c
@@ -8,17 +8,17 @@ static uint16_t switch_pins[] = {SW_PIN_2, SW_PIN_4, SW_PIN_3, SW_PIN_1};
 static size_t switch_pins_size = sizeof(switch_pins) / sizeof(switch_pins[0]);
 
 struct gpio_pin_cnf pin_sense_low = {
-    GPIO_DIR_INPUT,
-    GPIO_INPUT_CONNECT,
-    GPIO_PULL_PULLUP,
-    GPIO_SENSE_LOW,
+    GPIO_CNF_DIR_INPUT,
+    GPIO_CNF_INPUT_CONNECT,
+    GPIO_CNF_PULL_PULLUP,
+    GPIO_CNF_SENSE_LOW,
 };
 
 struct gpio_pin_cnf pin_sense_high = {
-    GPIO_DIR_INPUT,
-    GPIO_INPUT_CONNECT,
-    GPIO_PULL_PULLDOWN,
-    GPIO_SENSE_HIGH,
+    GPIO_CNF_DIR_INPUT,
+    GPIO_CNF_INPUT_CONNECT,
+    GPIO_CNF_PULL_PULLDOWN,
+    GPIO_CNF_SENSE_HIGH,
 };
 
 void init_switches() {
@@ -37,7 +37,7 @@ void init_switches() {
   for (int i = 0; i < switch_pins_size; i++) {
     GPIO0->DIR |= (0 << switch_pins[i]);
 
-    gpio_set_pin_cnf(GPIO0_BASE, switch_pins[i], &pin_sense_low);
+    gpio0_set_pin_cnf(switch_pins[i], &pin_sense_low);
   }
 
   // Enable LDETECT mode to be able to read which pins triggered the PORT event
