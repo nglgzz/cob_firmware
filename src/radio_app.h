63abd37c1366a47a43ce2fc5daba1ef9911e3332
Angelo Gazzola <angelo@nglgzz.com>
2025-05-20 08:49:22 +0200

    more cleanup, send switches state through radio

--- src/radio_app.h
+++
@@ -1,9 +0,0 @@
-#ifndef RADIO_APP_H
-
-#include "radio.h"
-
-void start_tx_loop();
-void start_rx_loop();
-
-#define RADIO_APP_H
-#endif  // RADIO_APP_H
---
+++ src/utils.c
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+void* memcpy(void* dest, const void* src, size_t n) {
+  char* d = (char*)dest;
+  char* s = (char*)src;
+
+  // Copy byte by byte
+  while (n--) {
+    *d++ = *s++;
+  }
+
+  return dest;
+}
